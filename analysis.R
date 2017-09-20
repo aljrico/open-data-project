574197d81ef17f0586fc2d031bbfd999c19a79c2
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-20 11:53:19 +0200

    Added test string

--- analysis.R
+++ analysis.R
@@ -9,6 +9,8 @@ library(tidytext) # Loads tidytetx
 # Sentiment analysis test ----------------------------------
 
 # The AFINN lexicon assigns words with a score that runs between -5 and 5, with negative scores indicating negative sentiment and positive scores indicating positive sentiment
-get_sentiments("afinn")
 
-# second test Crs
+# get_sentiments("afinn")
+
+test.text <- "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents."
+
