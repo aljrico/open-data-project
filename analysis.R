89c33720acf0be6d013d3dbe5de0caddcf98708f
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-20 10:11:59 +0200

    First commit

---
+++ analysis.R
@@ -0,0 +1,10 @@
+# Base code for the Open Data project
+# Authors: Cristian Estany <cresbabellpuig@gmail.com>
+#          Alfredo Hernández <aldomann.designs@gmail.com>
+#          Alejandro Jiménez <aljrico@gmail.com>
+
+library(tidytext)
+
+# Sentiment analysis test ----------------------------------
+
+
