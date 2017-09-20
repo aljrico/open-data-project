54a486a2b2746b60d359091e5f1413b224744622
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-20 12:27:16 +0200

    Added working sentiment analysis example

--- analysis.R
+++ analysis.R
@@ -5,12 +5,15 @@
 
 library(tidyverse)
 library(tidytext) # Loads tidytetx
+library(stringi)
 
 # Sentiment analysis test ----------------------------------
-
-# The AFINN lexicon assigns words with a score that runs between -5 and 5, with negative scores indicating negative sentiment and positive scores indicating positive sentiment
 
 # get_sentiments("afinn")
 
 test.text <- "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents."
 
+test.text <- data.frame(word = unlist(stri_extract_all_words(stri_trans_tolower(test.text))))
+
+test.text %>%
+	inner_join(get_sentiments("afinn"))
