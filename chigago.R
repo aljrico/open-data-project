bac125d28fd66eb3657a596cae29f61b7f88d8b8
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-21 23:42:27 +0200

    Added plot to test the data

--- chigago.R
+++ chigago.R
@@ -7,7 +7,11 @@
 
 library(tidyverse)
 
-# Read raw data --------------------------------------------
+# Read and clean raw data ----------------------------------
 
 chicago.df <- read.csv(file="data/Crimes_-_2001_to_present.csv",nrows=200)
 
+# Working example ------------------------------------------
+
+ggplot(chicago.df, aes(x = Longitude, y = Latitude)) +
+	geom_point(aes(colour = Primary.Type))
