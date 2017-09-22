07a78d92de816e3b75a82a39fac125214209ca7e
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-22 02:06:24 +0200

    Select only pertinent columns of the raw data

--- chigago.R
+++ chigago.R
@@ -6,10 +6,20 @@
 # Libraries ------------------------------------------------
 
 library(tidyverse)
+library(data.table)
 
 # Read and clean raw data ----------------------------------
 
-chicago.df <- read.csv(file="data/Crimes_-_2001_to_present.csv",nrows=200)
+# All data (a few rows, for testing purposes)
+# chicago.df <- read.csv(file="data/Crimes_-_2001_to_present_clean.csv", nrows = 200)
+
+# Read interesting columns and save into a CSV
+# chicago.df.clean <- fread("data/Crimes_-_2001_to_present.csv", sep = ",", header= TRUE, select = c(3,6,8,9,10,20,21))
+# write_csv(chicago.df.clean, "data/Crimes_-_2001_to_present_clean.csv")
+
+# Read cleaned data
+chicago.df <- fread(file="data/Crimes_-_2001_to_present_clean.csv", sep = ",", header = TRUE) %>%
+	rename(Primary.Type = `Primary Type` , Location.Description = `Location Description`)
 
 # Working example ------------------------------------------
 
