03487fafed2eed366b2c6f27d7f3bec4fdf3a759
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-25 11:00:43 +0200

    Conditional downloading/cleaning of data

--- chicago.R
+++ chicago.R
@@ -12,12 +12,19 @@ library(ggmap)
 
 # Read and clean raw data ----------------------------------
 
-# All data (a few rows, for testing purposes)
-# chicago.df <- read.csv(file="data/Crimes_-_2001_to_present_clean.csv", nrows = 200)
+# Download raw data
+crimes.file = "data/Crimes_-_2001_to_present.csv"
+if (!file.exists(crimes.file)) {
+	# SRC: https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/ijzp-q8t2
+	download.file("https://data.cityofchicago.org/api/views/ydr8-5enu/rows.csv", destfile = crimes.file)
+}
 
 # Read interesting columns and save into a CSV
-# chicago.df.clean <- fread("data/Crimes_-_2001_to_present.csv", sep = ",", header= TRUE, select = c(3,6,8,9,10,20,21))
-# write_csv(chicago.df.clean, "data/Crimes_-_2001_to_present_clean.csv")
+crimes.file.clean = "data/Crimes_-_2001_to_present_clean.csv"
+if (file.exists(crimes.file) & !file.exists(crimes.file.clean)) {
+	chicago.df.clean <- fread(crimes.file, sep = ",", header= TRUE, select = c(3,6,8,9,10,20,21))
+	write_csv(chicago.df.clean, crimes.file.clean)
+}
 
 # Read cleaned data
 chicago.df <- fread(file="data/Crimes_-_2001_to_present_clean.csv", sep = ",", header = TRUE)
@@ -39,7 +46,10 @@ chicago.df <- chicago.df %>%
 	filter(year(Date) %in% 2001:2017)
 
 # Smaller data frame for testing purposes
-chicago.df.small <- chicago.df[1:2000,]
+chicago.df.small <- chicago.df[1:2000,] %>%
+	mutate(Year = year(Date))
+
+sort(table(chicago.df$Primary.Type))
 
 # Load map data --------------------------------------------
 
@@ -53,14 +63,14 @@ chicago.map <- ggmap(chicago, base_layer = ggplot(
 
 ggmap(chicago) +
 	geom_point(data = chicago.df.small, aes(x = Longitude, y = Latitude, colour = Primary.Type)) +
-	labs(x = "Longitude", y = "Latitude")
+	labs(x = "Longitude", y = "Latitude") +
+	facet_wrap(~ year(chicago.df.small[,Date]))
 
 # Random plots ---------------------------------------------
 
 # Histograms
 ggplot(chicago.df %>% filter(Primary.Type == "THEFT" | Primary.Type == "BURGLARY")) +
-	geom_bar(aes(wday(Date, label = T))) +
-	facet_wrap(~ year(Date))
+	geom_bar(aes(wday(Date, label = T)))
 
 ggplot(chicago.df %>% filter(Primary.Type == "THEFT" | Primary.Type == "BURGLARY")) +
 	geom_bar(aes(wday(Date, label = T)))
@@ -76,5 +86,5 @@ chicago.map +
 	stat_density2d(aes(fill = ..level.., alpha = ..level..), size = 0.01,
 								 bins = 16, geom = "polygon") +
 	scale_fill_gradient(low = "green", high = "red") +
-	scale_alpha(range = c(0, 0.3), guide = FALSE)
-	# facet_wrap(~ day(chicago.df.small$Date))
+	scale_alpha(range = c(0, 0.3), guide = FALSE) +
+	facet_wrap(~ Year)
