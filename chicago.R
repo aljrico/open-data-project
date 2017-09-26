eadd3ef49bda04a862d6da46aeb10174cb4e48c4
Alfredo Hernández <aldomann.designs@gmail.com>
2017-09-26 13:11:33 +0200

    Smarter cleaning process & meta categories

--- chicago.R
+++ chicago.R
@@ -6,6 +6,7 @@
 # Libraries ------------------------------------------------
 
 library(tidyverse)
+library(plyr)
 library(data.table)
 library(lubridate)
 library(ggmap)
@@ -22,35 +23,67 @@ if (!file.exists(crimes.file)) {
 # Read interesting columns and save into a CSV
 crimes.file.clean = "data/Crimes_-_2001_to_present_clean.csv"
 if (file.exists(crimes.file) & !file.exists(crimes.file.clean)) {
-	chicago.df.clean <- fread(crimes.file, sep = ",", header= TRUE, select = c(3,6,9,18,20,21))
-	write_csv(chicago.df.clean, crimes.file.clean)
+	chicago.df <- fread(crimes.file, sep = ",", header= TRUE, select = c(3,6,9,18,20,21))%>%
+		# Clean data names and
+		dplyr::rename(PrimaryType = `Primary Type`) %>%
+		filter(is.na(Longitude) != TRUE) %>%
+		mutate(PrimaryType = ifelse(grepl("NON", PrimaryType), "NON-CRIMINAL", PrimaryType)) %>%
+		# Create meta crime category
+		mutate(Category = PrimaryType) %>%
+		mutate(Category = plyr::mapvalues(Category,
+																			dummy <- c("ARSON", "BURGLARY", "CRIMINAL DAMAGE", "CRIMINAL TRESPASS",
+																								 "MOTOR VEHICLE THEFT", "THEFT"),
+																			c(rep("PROPERTY CRIME", length(dummy))))) %>%
+		mutate(Category = plyr::mapvalues(Category,
+																			dummy <- c("ASSAULT", "BATTERY", "CRIM SEXUAL ASSAULT", "DOMESTIC VIOLENCE",
+																								 "HOMICIDE", "HUMAN TRAFFICKING", "INTIMIDATION", "KIDNAPPING",
+																								 "ROBBERY", "SEX OFFENSE", "STALKING", "WEAPONS VIOLATION"),
+																			c(rep("VIOLENT CRIME", length(dummy))))) %>%
+		mutate(Category = plyr::mapvalues(Category,
+																			dummy <- c("CONCEALED CARRY LICENSE VIOLATION", "DECEPTIVE PRACTICE", "GAMBLING",
+																								 "INTERFERENCE WITH PUBLIC OFFICER", "LIQUOR LAW VIOLATION", "NARCOTICS",
+																								 "NON-CRIMINAL", "OBSCENITY", "OFFENSE INVOLVING CHILDREN",
+																								 "OTHER NARCOTIC VIOLATION",  "OTHER OFFENSE", "PROSTITUTION",
+																								 "PUBLIC INDECENCY", "PUBLIC PEACE VIOLATION", "RITUALISM"),
+																			c(rep("QUALITY OF LIFE CRIME", length(dummy))))) %>%
+		dplyr::select(PrimaryType, Category, Date, Year, Arrest, Longitude, Latitude)
+
+	write_csv(chicago.df, crimes.file.clean)
+	rm(chicago.df)
 }
 
-# Read cleaned data
-chicago.df <- fread(file="data/Crimes_-_2001_to_present_clean.csv", sep = ",", header = TRUE)
-
-# Clean formats and column names
-chicago.df <- chicago.df %>%
-	rename(Primary.Type = `Primary Type`) %>%
-	mutate(Date = mdy_hms(Date)) %>%
-	filter(is.na(Longitude) != TRUE) %>%
-	mutate(Primary.Type = ifelse(grepl("NON", Primary.Type), "NON-CRIMINAL", Primary.Type))
-
-unique(chicago.df$Primary.Type)
-sort(table(chicago.df$Primary.Type))
+rm(crimes.file, crimes.file.clean)
 
 # NA test (only 1.3% of the reports are not geolocated)
 # summary(chicago.df$Latitude)
 
+# Read already cleaned data --------------------------------
+
+# Read cleaned data
+chicago.df <- fread(file="data/Crimes_-_2001_to_present_clean.csv", sep = ",", header = TRUE)
+
+# Use proper lubridate format
+chicago.df <- chicago.df %>%
+	mutate(Date = mdy_hms(Date))
+
 # Filter an arbitrary range of years
 chicago.df <- chicago.df %>%
-	filter(year(Date) %in% 2001:2017)
+	filter(Year %in% 2010:2017)
 
 # Smaller data frame for testing purposes
-chicago.df.small <- chicago.df[1:2000,] %>%
-	mutate(Year = year(Date))
+chicago.df.small <- chicago.df[1:20,]
 
-sort(table(chicago.df$Primary.Type))
+# Summarised data frame
+chicago.by.cat <- chicago.df %>%
+	group_by(Category, Year, Month = month(Date)) %>%
+	summarise(N = n()) %>%
+	mutate(Date = ymd(paste(Year, Month, 1))) %>%
+	ungroup() %>%
+	select(-c(Year, Month))
+
+# Chicago Tribune-like plot
+ggplot(chicago.by.cat) +
+	geom_line(aes(x = Date, y = N, colour = Category))
 
 # Load map data --------------------------------------------
 
@@ -58,22 +91,22 @@ chicago <- get_map(location = "Chicago, Illinois", zoom = 11, source = "google")
 
 chicago.map <- ggmap(chicago, base_layer = ggplot(
 	aes(x = Longitude, y = Latitude),
-	data = chicago.df.small))
+	data = chicago.df))
 
 # Maps -----------------------------------------------------
 
 ggmap(chicago) +
-	geom_point(data = chicago.df.small, aes(x = Longitude, y = Latitude, colour = Primary.Type)) +
+	geom_point(data = chicago.df.small, aes(x = Longitude, y = Latitude, colour = Category)) +
 	labs(x = "Longitude", y = "Latitude") +
-	facet_wrap(~ year(chicago.df.small[,Date]))
+	facet_wrap(~ Year)
 
 # Random plots ---------------------------------------------
 
 # Histograms
-ggplot(chicago.df %>% filter(Primary.Type == "THEFT" | Primary.Type == "BURGLARY")) +
+ggplot(chicago.df %>% filter(PrimaryType == "THEFT" | PrimaryType == "BURGLARY")) +
 	geom_bar(aes(wday(Date, label = T)))
 
-ggplot(chicago.df %>% filter(Primary.Type == "THEFT" | Primary.Type == "BURGLARY")) +
+ggplot(chicago.df %>% filter(PrimaryType == "THEFT" | PrimaryType == "BURGLARY")) +
 	geom_bar(aes(wday(Date, label = T)))
 
 # Testing lubdridate
@@ -82,10 +115,12 @@ ggplot(chicago.df.small, aes(Date, Longitude)) +
 
 # Heatmaps -------------------------------------------------
 
-chicago.map +
+ggmap(chicago, base_layer = ggplot(aes(x = Longitude, y = Latitude), data = chicago.df)) +
 	geom_density2d(size = 0.3) +
 	stat_density2d(aes(fill = ..level.., alpha = ..level..), size = 0.01,
 								 bins = 16, geom = "polygon") +
 	scale_fill_gradient(low = "green", high = "red") +
-	scale_alpha(range = c(0, 0.3), guide = FALSE) +
+	scale_alpha(range = c(0, 0.3), guide = FALSE)
+
++
 	facet_wrap(~ Year)
